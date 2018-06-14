a6c6d6a63304fbdf8e17dd648709669a1c0c695d
David Kaller <rexrayo15@gmail.com>
2018-06-14 12:57:48 -0400

    Se modificaron los modelos

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+	has_and_belongs_to_many :meeting
+
+def meetings
+	@meetings = Meeting.find(1)
+
+	return @meetings
+end
+
+
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,10 @@
 class Meeting < ApplicationRecord
+	has_and_belongs_to_many :employee
+
+	def employees
+		@employees = Employee.find(1)
+
+	return @employees
+		
+	end
 end
