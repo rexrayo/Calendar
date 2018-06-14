252541fe4fc63d604fc58e848fa23cf10dfb5524
David Kaller <rexrayo15@gmail.com>
2018-06-14 12:58:35 -0400

    Se modifico la ruta por defecto

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   get 'index' => 'welcome#index'
   # get 'welcome/index'
   get 'welcome/new'
-  root 'welcome#new'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
